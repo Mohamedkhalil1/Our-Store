0e6097eed2268124bde396f6552ba3efa4fc9300
unknown <mohamedkhalil125445@gmail.com>
2020-08-19 16:38:29 +0200

    start

---
+++ app/Helpers/General.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Language;
+use Illuminate\Support\Facades\Config;
+
+function get_languages(){
+
+    return Language::active()->selection()->get();
+}
+
+function get_default_lang(){
+    return Config::get('app.locale');
+}
+
+function uploadImage($folder,$image){
+    $image->store('/',$folder);
+    $filename= $image->hashName();
+    $path = 'images/'.$folder.'/'.$filename;
+    return $path;
+}
