05e0f3f497038a8580f5062d17edf1ebc34202f3
unknown <mohamedkhalil125445@gmail.com>
2020-09-21 01:03:59 +0200

    added brands crud

---
+++ app/Models/Brand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use Translatable;
+
+    protected $with = ['translations'];
+
+    protected $fillable =['is_active','photo'];
+
+    protected $translatedAttributes = ['name'];
+
+    protected $hidden = ['translations'];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    public function getActive(){
+        return   $this->is_active == 1 ? 'مفعل'  : 'غير مفعل';
+    }
+
+    public function getPhotoAttribute($val){
+        return ($val !== null) ? asset('assets/'.$val) : '' ;
+    }
+}
